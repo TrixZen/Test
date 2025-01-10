525f0858d3039ea42ca335305f78a81ac58bfb6d
GDYolk <eolk1230@gmail.com>
2025-01-10 16:59:45 +0800

    Print class үүсгэсэн

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,5 +3,10 @@ package main;
 public class Main {
     public static void main(String[] args) {
         Print.printer("Hello world");
+        System.out.println(Main.calculateSum(5, 10));
+        System.out.println(Main.calculateSum(7, 15));
+    }
+    private static int calculateSum(int a, int b) {
+        return a + b ;
     }
 }
